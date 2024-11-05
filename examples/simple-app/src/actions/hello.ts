47cd25b569b282c267d54f589e190d6a641ca3d5
Roy <roy@sommer.co.il>
2024-11-05 16:55:58 +0200

    added eslint rules to ensure no cross importing within the runtime package

--- examples/simple-app/src/actions/hello.ts
+++ examples/simple-app/src/actions/hello.ts
@@ -1,5 +1,6 @@
 import { randomUUID } from 'node:crypto';
 
 export async function hello() {
+  console.log('hello from main');
   return randomUUID();
 }
--- packages/runtime/eslint.config.mjs
+++ packages/runtime/eslint.config.mjs
@@ -1,3 +1,39 @@
 import eslintConfig from '@react-appkit/eslint-config';
 
-export default [...eslintConfig];
+export default [
+  ...eslintConfig,
+  {
+    files: ['src/main/**/*.{ts,tsx}', 'src/entrypoints/main.ts'],
+    rules: {
+      'no-restricted-imports': [
+        'error',
+        {
+          patterns: [
+            {
+              group: ['**/renderer/**/*'],
+              message:
+                '\nCannot import code that is meant for the renderer process.',
+            },
+          ],
+        },
+      ],
+    },
+  },
+  {
+    files: ['src/renderer/**/*.{ts,tsx}'],
+    rules: {
+      'no-restricted-imports': [
+        'error',
+        {
+          patterns: [
+            {
+              group: ['../**/main/**/*'],
+              message:
+                '\nCannot import code that is meant for the main process.\nThe only exception is the "main/api" collection, and in order to import it, you must do so explicitly via "@react-appkit/runtime/main/api/*".',
+            },
+          ],
+        },
+      ],
+    },
+  },
+];
