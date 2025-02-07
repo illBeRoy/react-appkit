7617b86d165dcfea9eb3a280e8c9ec1341558377
Roy <roy@sommer.co.il>
2025-02-07 16:09:15 +0200

    added placeholder package

---
+++ packages/react-appkit/src/index.ts
@@ -0,0 +1,6 @@
+console.log(
+  'This is a placeholder package! If you want to start using React AppKit, run:',
+);
+console.log('  npm create react-appkit-app');
+console.log('Or visit our github repository at:');
+console.log('  https://github.com/illberoy/react-appkit');
